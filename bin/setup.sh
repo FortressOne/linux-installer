9bff3576f65092546e187fac782a4673ff0e6cf9
Sheldon Johnson <shayolden@hotmail.com>
2018-10-21 23:42:12 +1100

    shellcheck compliance

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,22 +7,19 @@ error() {
 }
 
 # Check for install dependencies
-[ `which unzip` ] || error "ERROR: The package 'unzip' is not installed. Please install it and run the installation again."
-[ `which tar` ] || error "ERROR: The package 'tar' is not installed. Please install it and run the installation again."
-[ `which wget` ] || error "ERROR: The package 'wget' is not installed. Please install it and run the installation again."
-[ `which desktop-file-install` ] || error "ERROR: The package 'desktop-file-install' is not installed. Please install it and run the installation again."
+[ "$(which unzip)" ] || error "ERROR: The package 'unzip' is not installed. Please install it and run the installation again."
+[ "$(which tar)" ] || error "ERROR: The package 'tar' is not installed. Please install it and run the installation again."
+[ "$(which wget)" ] || error "ERROR: The package 'wget' is not installed. Please install it and run the installation again."
+[ "$(which desktop-file-install)" ] || error "ERROR: The package 'desktop-file-install' is not installed. Please install it and run the installation again."
 
 TARGET="$HOME/bin/fortressone"
 
-printf "Choose installation directory [${TARGET}]: "
-read INSTALL_DIR
+printf "Choose installation directory [%s]: " "$TARGET"
+read -r INSTALL_DIR
 
-if [ ! -z $INSTALL_DIR ]; then
+if [ ! -z "$INSTALL_DIR" ]; then
   TARGET=$INSTALL_DIR
 fi
-
-echo $TARGET
-error "exit"
 
 # get install directory (default ~/bin/fortressone)
 echo "Installing to $TARGET"
@@ -33,7 +30,7 @@ if [ -d "$TARGET" ]; then
 fi
 
 # Creating installation directory
-mkdir -p $TARGET
+mkdir -p "$TARGET"
 
 # Check directory succesfully created
 if [ ! -d "$TARGET" ]; then
@@ -41,7 +38,7 @@ if [ ! -d "$TARGET" ]; then
 fi
 
 # Installing logo
-cp logo.png $TARGET
+cp logo.png "$TARGET"
 
 # Checking for .desktop file location
 if [ ! -d "/usr/share/applications" ]; then
@@ -58,31 +55,31 @@ sudo desktop-file-install fortressone.desktop
 # get the ezquake linux package
 wget https://github.com/ezQuake/ezquake-source/releases/download/3.1/ezquake3.1-linux64-full.tar.gz
 # untar to install directory
-tar -xvf ezquake3.1-linux64-full.tar.gz -C $TARGET
+tar -xvf ezquake3.1-linux64-full.tar.gz -C "$TARGET"
 
 # get ezquake31 binary compiled against Ubuntu 18.04
 wget https://s3-ap-southeast-2.amazonaws.com/qwtf/ezquake31/ezquake-linux-x86_64
 # copy to install dir
-cp ezquake-linux-x86_64 $TARGET
+cp ezquake-linux-x86_64 "$TARGET"
 
 # get fortressone media files
 wget https://s3-ap-southeast-2.amazonaws.com/qwtf/fortress-one-gfx.zip
 # unzip to install directory
-unzip fortress-one-gfx.zip -d $TARGET
+unzip fortress-one-gfx.zip -d "$TARGET"
 
 # get shareware quake pak0.pak
 mkdir id1/
 wget https://s3-ap-southeast-2.amazonaws.com/qwtf/paks/id1/pak0.pak -P id1/
 # copy to install dir
-cp id1/pak0.pak $TARGET/id1/
+cp id1/pak0.pak "$TARGET/id1/"
 
 # get tf2.9 fortress.pak
 mkdir fortress/
 wget https://s3-ap-southeast-2.amazonaws.com/qwtf/paks/fortress/pak0.pak -P fortress/
 # copy to install dir
-cp fortress/pak0.pak $TARGET/fortress
+cp fortress/pak0.pak "$TARGET/fortress"
 
 # get cfgs
 wget https://github.com/FortressOne/fortress-one-cfgs/archive/master.zip
 unzip master.zip
-cp -r fortress-one-cfgs-master/* $TARGET
+cp -r fortress-one-cfgs-master/* "$TARGET"
