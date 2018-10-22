af514e1e25f0358415c1c933a03c8aa5babbec97
Sheldon Johnson <shayolden@hotmail.com>
2018-10-23 00:00:00 +1100

    Add build script

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 VERSION="0.2.1"
 
 makeself bin/ "fortressone-${VERSION}.run" 'FortressOne - A QuakeWorld Team Fortress installer' ./setup.sh
+
+cp README_TEMPLATE.md README.md
+sed -i "s|VERSION|${VERSION}|g" README.md
