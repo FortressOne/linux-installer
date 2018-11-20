934f864619a600f85ccad788d5db74cc4f11c6bf
Sheldon Johnson <shayolden@hotmail.com>
2018-11-21 02:08:01 +1100

    Rename installer file

--- README_TEMPLATE.md
+++ README_TEMPLATE.md
@@ -7,7 +7,7 @@ Currently tested on Ubuntu 18.04. Doesn't work on your system? [Raise an issue](
 Download [FortressOne](https://github.com/FortressOne/linux-installer/releases/latest)
 
 ```bash
-$ sh fortressone-VERSION.run
+$ sh fortressone-linux-installer-VERSION.run
 ```
 
 
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 VERSION="1.0.0"
 
-makeself bin/ "fortressone-${VERSION}.run" 'FortressOne - A QuakeWorld Team Fortress package' ./setup.sh
+makeself bin/ "fortressone-linux-installer-${VERSION}.run" 'FortressOne - A QuakeWorld Team Fortress package' ./setup.sh
 
 cp README_TEMPLATE.md README.md
 sed -i "s|VERSION|${VERSION}|g" README.md
