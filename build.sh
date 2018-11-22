d761fe0356ce7852ac2dcd55f432bea33b27f834
Sheldon Johnson <shayolden@hotmail.com>
2018-11-22 18:15:33 +1100

    Use consistent tag line

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 VERSION="1.0.0"
 
-makeself bin/ "fortressone-linux-installer-${VERSION}.run" 'FortressOne - A QuakeWorld Team Fortress package' ./setup.sh
+makeself bin/ "fortressone-linux-installer-${VERSION}.run" 'FortressOne - QuakeWorld Team Fortress package for Linux' ./setup.sh
 
 cp README_TEMPLATE.md README.md
 sed -i "s|VERSION|${VERSION}|g" README.md
